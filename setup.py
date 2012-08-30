2ffa226c80834d4039edfea5a746679472ae5176
Huan Do <doboy0@gmail.com>
2012-08-29 21:19:22 -0400

    ready to publish

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name="Also",
+    name="also",
     version="0.0.1",
-    packages=["also"]
+    packages=["also"],
     author="Huan Do",
     author_email="doboy0@gmail.com",
     )
